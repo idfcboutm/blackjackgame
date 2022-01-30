0ae18d8ad9f819e1b9a6fc368ec570583cb8ea4d
Jannis Wittchen <janni201103@gmail.com>
2022-01-30 20:53:57 +0100

    added cards

--- cards.py
+++ cards.py
@@ -1,2 +1,14 @@
-cards = []
+symbol = ["Spades", "Hearts", "Clubs", "Diamonds"]
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+spade_cards = cards
+heart_cards = cards
+heart_cards = cards
+diamond_cards = cards
+
+print(spade_cards)
+print(heart_cards)
+print(heart_cards)
+print(diamond_cards)
+
+
 played_cards = []
