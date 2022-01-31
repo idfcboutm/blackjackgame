382cfcb05f074d84454ec6c29d2de33e1e2d7998
Jannis Wittchen <janni201103@gmail.com>
2022-01-31 18:33:55 +0100

    added more functions like card_count

--- cards.py
+++ cards.py
@@ -1,9 +1,9 @@
-symbol = ["Spades", "Hearts", "Clubs", "Diamonds"]
+symbol = ["Spade", "Heart", "Club", "Diamond"]
 cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-spade_cards = cards
-heart_cards = cards
-heart_cards = cards
-diamond_cards = cards
+spade_card = cards
+heart_card = cards
+club_card = cards
+diamond_card = cards
 
 
 played_cards = []
